cba6a88736bbd8816cf4d4df699e8a8b8c675428
Omar <omar3568@gmail.com>
2023-04-13 01:30:27 -0500

    Ejercicio de Plantas con POO

---
+++ src/plantas/Plantas.java
@@ -0,0 +1,17 @@
+
+package plantas;
+
+public class Plantas {
+    public static void main(String args[]){
+        
+        Arbol arbol = new Arbol();
+        Flor flor = new Flor();
+        Arbusto arbusto = new Arbusto();
+        
+        arbol.decirLoQueSoy();
+        flor.decirLoQueSoy();
+        arbusto.decirLoQueSoy();
+        
+        
+    }
+}
